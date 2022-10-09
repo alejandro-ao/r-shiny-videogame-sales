a8c3f3c9fb48aa18e8c2823e83aa2cea7e50e4ca
Alejandro <aavila0707@gmail.com>
2022-10-09 16:49:10 +0200

    enable toggle for log scale

--- app.R
+++ app.R
@@ -25,6 +25,7 @@ ui <- fluidPage(
         "Japan",
         "Rest of the world"
       )),
+      checkboxInput("enable_log_scale", label="See log scale", value=TRUE)
     ),
     
     # Show a plot of the generated distribution
@@ -52,20 +53,26 @@ server<-function (input,output){
                      "Japan" = vgsales$JP_Sales,
                      "Rest of the world" = vgsales$Other_Sales
                      )
-    plot_title <- paste(input$y_axis, "Sales by", input$x_axis)
+    plot_title <- paste(input$y_axis, "Videogame Sales by", input$x_axis)
     
-    ggplot(vgsales,
+    p <- ggplot(vgsales,
            aes(
              x= reorder(x_axis,
                         y_axis,
                         median),
              y=y_axis)) +
-      geom_boxplot() +
+      geom_boxplot(color="#47B5FF", fill="white", alpha=0.4) +
       labs(title=plot_title) +
       xlab(input$x_axis) + 
       ylab(paste(input$y_axis, "sales (in millions)")) +
-      scale_y_log10() +
       theme(axis.text.x = element_text(angle = 90))
+    
+    if (input$enable_log_scale) {
+      p + scale_y_log10()
+    } else {
+      p
+    }
+    
   })
 }
 
